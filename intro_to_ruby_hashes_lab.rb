22b00f758ab04d0021269ec2602768fd6571809d
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-11 11:19:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,26 +4,23 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
   new = {:name => "Andy"}
   return new
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   pioneer = {:name => 'Grace Hopper'}
   return pioneer
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
   id = {:id => 7}
   return id
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-  
+  newHash = {:key => value}
+  return newHash
 end
 
 def read_from_hash(hash, key)
